2157a59d067db8fedf7145378cbd84aa28304a2e
yangjianwei <2017585616@qq.com>
2021-02-04 01:28:53 +0800

    测试实例化和初始化

---
+++ src/main/java/com/deepblue/inaction/chapter_02_safe/thread_004_InnerLock/SynchronizedTest.java
@@ -0,0 +1,11 @@
+package com.deepblue.inaction.chapter_02_safe.thread_004_InnerLock;
+
+public class SynchronizedTest {
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
---
+++ src/main/java/com/deepblue/inaction/mine/TestObject.java
@@ -0,0 +1,10 @@
+package com.deepblue.inaction.mine;
+
+public class TestObject {
+
+    public static void main(String[] args) {
+        User user = new User();
+        System.out.println("username :" + user.getUsername());
+    }
+
+}
