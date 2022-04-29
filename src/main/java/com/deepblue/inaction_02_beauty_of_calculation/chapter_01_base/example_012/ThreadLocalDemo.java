4cf50cb90add309c886526d9a3f40270f8ce878f
yangjianwei <2017585616@qq.com>
2022-04-29 17:20:07 +0800

    threadlocal使用

--- src/main/java/com/deepblue/inaction_02_beauty_of_calculation/chapter_01_base/example_012/ThreadLocalDemo.java
+++ src/main/java/com/deepblue/inaction_02_beauty_of_calculation/chapter_01_base/example_012/ThreadLocalDemo.java
@@ -1,15 +1,23 @@
 package com.deepblue.inaction_02_beauty_of_calculation.chapter_01_base.example_012;
 
+import com.alibaba.fastjson.JSON;
+
 public class ThreadLocalDemo {
 
-	public static void main(String[] args) {
+	public static ThreadLocal<String> threadLocal = new ThreadLocal<>();
 
-		ThreadLocal<String> threadLocal = new ThreadLocal<>();
+	public static void main(String[] args) throws InterruptedException {
+
 		Thread threadA = new Thread(new ThreadA(threadLocal), "threadA");
 		Thread threadB = new Thread(new ThreadA(threadLocal), "threadB");
 
 		threadA.start();
 		threadB.start();
+
+		threadA.join();
+		threadB.join();
+
+		System.out.println(JSON.toJSONString(threadLocal));
 
 		System.out.println("thread main method invoke end");
 	}
